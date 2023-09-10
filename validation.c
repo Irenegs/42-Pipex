40dadafc65ab84a2cd00ca1bc2c2d6d2b300dc56
Irenegs <gonzalezsaavedrairene@gmail.com>
2023-09-10 23:17:20 +0200

    Falta sleep y errores

--- validation.c
+++ validation.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   validation.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: irgonzal <irgonzal@student.42.fr>          +#+  +:+       +#+        */
+/*   By: irgonzal <irgonzal@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/30 17:36:54 by irgonzal          #+#    #+#             */
-/*   Updated: 2023/09/10 13:52:29 by irgonzal         ###   ########.fr       */
+/*   Updated: 2023/09/10 22:43:43 by irgonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,7 +54,6 @@ char    *get_path(char *s, int i, char **envp)
     char    *route;
     char    *folder;
 
-    //printf("PATH:%s\n", envp[2]);
     route = NULL;
     if (s && select_variable(envp) != -1)
     {
@@ -70,9 +69,7 @@ char    *get_path(char *s, int i, char **envp)
                 free(folder);
             }
         }
-        //printf("get path\n");
         free_list(path);
-        //printf("after free\n");
     }
     return (route);
 }
@@ -84,16 +81,13 @@ int command_exists(char *s, char **envp)
 
     if (s)
     {
-        //printf("command exists\n");
         i = 1;
         route = get_path(s, i, envp);
         while (route != NULL)
         {
-            //printf("i:[%d-%s]\n", i, route);
             if (access(route, F_OK) == 0 && access(route, X_OK) == 0)
             {
                 free(route);
-                //printf("ret i:[%d]\n", i);
                 return (i);
             }
             free(route);
@@ -128,10 +122,8 @@ int validation(int argc, char **argv, char **envp)
     {
         if (argc == 5 && validation_files(argc, argv) == 0)
         {
-            //printf("files ok\n");
             if (c1 && command_exists(c1[0], envp) != -1)
             {
-                //printf("c1 ok\n");
                 if (c2 && command_exists(c2[0], envp) != -1)
                 {
                     free_list(c1);
